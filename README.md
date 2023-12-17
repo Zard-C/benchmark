92a20513527b633418cc88642619c076eda78268
Zard-C <patrick.zhang5233@gmail.com>
2023-12-17 23:14:12 +0800

    feat(rusage utils)
    Signed-off-by: Zard-C <patrick.zhang5233@gmail.com>

--- README.md
+++ README.md
@@ -3,6 +3,6 @@ Benchmark utils in case that we can't use google/benchmark
 
 ## Context
 
-- [ ] rusage utils
-- [ ] short function to burn cpu for some ns
+- [ ] rusage_utils.hpp: rusage utils
+- [ ] burn_cpu_cycles.hpp: short function to burn cpu for some ns
 - [ ] command to check process's tid, cpu usage, memory usage.
---
+++ include/rusage_utils.hpp
@@ -0,0 +1,106 @@
+#ifndef __RUSAGE_UTILS_HPP__
+#define __RUSAGE_UTILS_HPP__
+
+#include <sys/resource.h>
+#include <unistd.h>
+#include <cstdint>
+#include <mutex>
+
+namespace benchmark
+{
+
+int64_t get_involuntary_context_switches(int who = RUSAGE_THREAD) noexcept
+{
+  struct rusage rusage {};
+  getrusage(who, &rusage);
+  return static_cast<int64_t>(rusage.ru_nivcsw);
+}
+
+class ContextSwitchesCounter
+{
+public:
+  explicit ContextSwitchesCounter(int who = RUSAGE_THREAD)
+  : who_{who} {}
+
+  void init() noexcept
+  {
+    involuntary_context_switches_previous_ = get_involuntary_context_switches(who_);
+  }
+
+  [[nodiscard]] int64_t get() noexcept
+  {
+    std::call_once(
+      once_, [this] {init();}
+    );
+    int64_t current = get_involuntary_context_switches(who_);
+    return current - std::exchange(involuntary_context_switches_previous_, current);
+  }
+
+private:
+  int64_t involuntary_context_switches_previous_;
+  const int who_;
+  std::once_flag once_;
+};
+
+struct PageFaults
+{
+  std::int64_t minor{0};
+  std::int64_t major{0};
+};
+
+std::size_t get_process_memory(int who = RUSAGE_SELF) noexcept
+{
+  struct rusage rusage {};
+  getrusage(who, &rusage);
+  return static_cast<std::size_t>(rusage.ru_maxrss);
+}
+
+void print_process_memory(const std::string & log_text) noexcept
+{
+  printf("%s %ld MB\n", log_text.c_str(), get_process_memory() / 1024);
+}
+
+PageFaults get_total_page_faults(int who = RUSAGE_SELF) noexcept
+{
+  struct rusage rusage {};
+  getrusage(who, &rusage);
+  PageFaults new_page_faults{rusage.ru_minflt, rusage.ru_majflt};
+  return new_page_faults;
+}
+
+void print_page_faults(const std::string & log_text) noexcept
+{
+  auto pf = get_total_page_faults();
+  printf("%s [Minor: %ld, Major: %ld]\n", log_text.c_str(), pf.minor, pf.major);
+}
+
+class PageFaultCounter
+{
+public:
+  explicit PageFaultCounter(int who = RUSAGE_SELF)
+  : who_{who} {}
+
+  void reset() noexcept
+  {
+    page_faults_previous_ = get_total_page_faults(who_);
+  }
+
+  [[nodiscard]] PageFaults get_page_faults_difference() noexcept
+  {
+    PageFaults current = get_total_page_faults(who_);
+    PageFaults diff;
+    diff.major = current.major - page_faults_previous_.major;
+    diff.minor = current.minor - page_faults_previous_.minor;
+    page_faults_previous_ = current;
+    return diff;
+  }
+
+private:
+  PageFaults page_faults_previous_;
+  const int who_;
+};
+
+}  // namespace benchmark
+
+
+#endif  // __RUSAGE_UTILS_HPP__
